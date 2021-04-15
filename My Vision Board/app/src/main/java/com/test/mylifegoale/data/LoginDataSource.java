1737d06f987249c7c00929e2355708cab9814e04
feagans <70381848+feagans@users.noreply.github.com>
2021-04-15 17:44:12 -0400

    Fixing merge - commit issue

--- My Vision Board/app/src/main/java/com/test/mylifegoale/data/LoginDataSource.java
+++ My Vision Board/app/src/main/java/com/test/mylifegoale/data/LoginDataSource.java
@@ -5,25 +5,21 @@ import com.test.mylifegoale.data.model.LoggedInUser;
 
 import java.io.IOException;
 
-/**
- * Class that handles authentication w/ login credentials and retrieves user information.
- */
 public class LoginDataSource {
 
-    public Result<LoggedInUser> login(String username, String password) {
-
-        try {
-            //MyApplication.getInstance().API.login(username, password);
-
-            LoggedInUser fakeUser =
-                    new LoggedInUser(
-                            java.util.UUID.randomUUID().toString(),
-                            "Jane Doe");
-            return new Result.Success<>(fakeUser);
-        } catch (Exception e) {
-            return new Result.Error(new IOException("Error logging in", e));
-        }
-    }
+//    public Result<LoggedInUser> login(String username, String password) {
+//
+//        try {
+//
+//            LoggedInUser fakeUser =
+//                    new LoggedInUser(
+//                            java.util.UUID.randomUUID().toString(),
+//                            "Jane Doe");
+//            return new Result.Success<>(fakeUser);
+//        } catch (Exception e) {
+//            return new Result.Error(new IOException("Error logging in", e));
+//        }
+//    }
 
     public void logout() {
         // TODO: revoke authentication
